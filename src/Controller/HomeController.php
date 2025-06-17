ed4b53b1e659fcae97235e2aeaa0adf5b29ffdb8
ssamko0911 <ssamko091184@gmail.com>
2025-06-17 11:02:11 +0100

    Add HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Form\Type\PasswordGeneratorFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route(
+        path: '/',
+        name: 'home',
+        methods: ['GET']
+    )]
+    public function index(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+
+    #[Route(
+        path: '/password/generate',
+        name: 'password_generate',
+        methods: ['GET']
+    )]
+    public function generatePassword(): Response
+    {
+        $form = $this->createForm(PasswordGeneratorFormType::class);
+
+        return $this->render('password/generate_password.html.twig', [
+            'header' => 'Generate Password',
+            'form' => $form->createView(),
+        ]);
+    }
+}
