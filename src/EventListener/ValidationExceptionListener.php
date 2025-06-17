7a0ec666dbbd8a4ccd377e9a1b0f69f8a1940c15
ssamko0911 <ssamko091184@gmail.com>
2025-06-17 12:17:15 +0100

    Add ValidationExceptionListener

---
+++ src/EventListener/ValidationExceptionListener.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+
+#[AsEventListener(event: KernelEvents::EXCEPTION, method: 'onKernelException')]
+class ValidationExceptionListener
+{
+    public function __construct(
+        private RouterInterface $router,
+        private RequestStack $requestStack,
+    )
+    {
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $exception = $exception->getPrevious();
+
+        if ($exception instanceof ValidationFailedException) {
+            $request = $event->getRequest();
+            $session = $request->getSession();
+
+            foreach ($exception->getViolations() as $violation) {
+                $session->getFlashBag()->add('error', $violation->getMessage());
+            }
+
+            $response = new RedirectResponse($this->router->generate('password_generate'));
+            $event->setResponse($response);
+        }
+    }
+}
